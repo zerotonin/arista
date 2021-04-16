2703fd3a56d2f4b903994a3138f213cd2848f5a7
Bart Geurten <bgeurte@gwdg.de>
2021-04-16 09:40:43 +0200

    test case for laurins stuff

---
+++ run4Laurin.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tempFileIO  as tIO
+import dill,os
+import matplotlib.pyplot as plt
+from importlib import reload  
+reload(tIO)
+
+genotype     = 'NompC3'
+gender       = 'm'
+celltype     = 'CC'
+cellnumber   = '1'
+stimulus     = 'ascAmp'
+saveDirTemp  = '~/ownCloud/personalSwaps/Laurin-Bart/resultDir'
+sourceDir    = '~/ownCloud/personalSwaps/Laurin-Bart/exampleDataSet'
+
+
+tIOobject  = tIO.tempFileIO(genotype,gender,stimulus,celltype,cellnumber,saveDir=saveDirTemp)
+#data = tIOobject.readInData(responseFpos,sensorFpos) 
+data = tIOobject.verboseMode(sourceDir)
+tIOobject.calcResponse()
+tIOobject.savePy()
+
+
+
