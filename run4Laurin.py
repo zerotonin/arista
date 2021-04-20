a5a45584f354d2c3c8924cd116764e2e48b6d90e
Bart Geurten <bgeurte@gwdg.de>
2021-04-21 00:59:13 +0200

    run ontqdm

--- run4Laurin.py
+++ run4Laurin.py
@@ -1,10 +1,11 @@
 import numpy as np
 import faulthandler
 import tempFileIO  as tIO
-import tciPlot 
+import tciPlot, tciAnalysis,autoMetaFinder
 import dill,os
 import matplotlib.pyplot as plt
 from importlib import reload  
+from tqdm import tqdm
 import pandas as pd
 reload(tIO)
 
@@ -24,8 +25,46 @@ data = tIOobject.verboseMode(sourceDir,responseExt='*.csv')
 #tIOobject.calcResponse()
 #tIOobject.savePy()
 df = tIOobject.prepPandas()
+
+reload(tciAnalysis)
+tAna = tciAnalysis.tciAnalysis(df)
+tAna.driftCorrection()
+
+reload(tciPlot)
+tPLT = tciPlot.tciPlot(df)
+tPLT.correctionSurvey(tAna.time_sec,tAna.df['deltaFbyF'],tAna.dfbf_expfit,tAna.dfbf_polyfit,tAna.dfbyf_expCorr,tAna.dfbyf_polyCorr)
+plt.show()
+
 tIOobject.savePandas()
 tPLT = tciPlot.tciPlot(df)
 tPLT.lineP_simpleSurvey()
 print('here')
 plt.show()
+
+
+import tempFileIO  as tIO
+from pathlib import Path
+import tciPlot, tciAnalysis,autoMetaFinder
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+sourceDir = '/media/gwdg-backup/BackUp/Laurin/ms-thesis/analysis_data2/'
+saveDir   = '/media/gwdg-backup/BackUp/Laurin/ms-thesis/result/'
+for path in tqdm(Path(sourceDir).rglob('*.csv'),desc='running...'):
+    aMF = autoMetaFinder.autoMetaFinder(path)
+    metaDict = aMF.run()
+    tIOobject  = tIO.tempFileIO(metaDict['strain'],
+                                metaDict['gender'],
+                                'adaptation',
+                                metaDict['cellType'],
+                                metaDict['cellNum'],
+                                saveDir=saveDir,
+                                fps = 10)
+    for matPath in Path(metaDict['expDir']).rglob('*.mat'):
+        tIOobject.readInData(str(path),str(matPath))
+        df = tIOobject.prepPandas()
+        tAna = tciAnalysis.tciAnalysis(df)
+        tAna.driftCorrection()
+        tPLT = tciPlot.tciPlot(df)
+        tPLT.correctionSurvey(tAna.time_sec,tAna.df['deltaFbyF'],tAna.dfbf_expfit,tAna.dfbf_polyfit,tAna.dfbyf_expCorr,tAna.dfbyf_polyCorr)
+        plt.show()
