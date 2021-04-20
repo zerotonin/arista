5735d7a1582ed8939db9dea08460620c095097f2
Bart Geurten <bgeurte@gwdg.de>
2021-04-20 13:04:14 +0200

    changed arround

--- run4Laurin.py
+++ run4Laurin.py
@@ -1,8 +1,11 @@
 import numpy as np
+import faulthandler
 import tempFileIO  as tIO
+import tciPlot 
 import dill,os
 import matplotlib.pyplot as plt
 from importlib import reload  
+import pandas as pd
 reload(tIO)
 
 genotype     = 'NompC3'
@@ -10,13 +13,19 @@ gender       = 'm'
 celltype     = 'CC'
 cellnumber   = '1'
 stimulus     = 'ascAmp'
-saveDirTemp  = '~/ownCloud/personalSwaps/Laurin-Bart/resultDir'
-sourceDir    = '~/ownCloud/personalSwaps/Laurin-Bart/exampleDataSet'
+saveDirTemp  = r'/home/bgeurten/ownCloud/personalSwaps/Laurin-Bart/resultDir'
+sourceDir    = r'/home/bgeurten/ownCloud/personalSwaps/Laurin-Bart/exampleDataSet'
 
 
+faulthandler.enable()
 tIOobject  = tIO.tempFileIO(genotype,gender,stimulus,celltype,cellnumber,saveDir=saveDirTemp)
 #data = tIOobject.readInData(responseFpos,sensorFpos) 
 data = tIOobject.verboseMode(sourceDir,responseExt='*.csv')
-tIOobject.calcResponse()
+#tIOobject.calcResponse()
 #tIOobject.savePy()
-
+df = tIOobject.prepPandas()
+tIOobject.savePandas()
+tPLT = tciPlot.tciPlot(df)
+tPLT.lineP_simpleSurvey()
+print('here')
+plt.show()
